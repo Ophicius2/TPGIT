ae4fb18cbc0cd8e674efcdba2e9ff6011a7703e4
Damien <d.gervy@free.fr>
2026-04-30 12:21:37 +0200

    added 1 Boutique and 2 Clients

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,10 +1,18 @@
 package nintendo.test;
 
+import nintendo.model.Adresse;
+import nintendo.model.Boutique;
+import nintendo.model.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Adresse adresseD = new Adresse("15", "rue de Mario", "Tokyo");
+		Boutique boutiqueD = new Boutique("Boutique du Lac", adresseD);
+		
+		Client clientD1 = new Client("Floyd", "George");
+		Client clientD2 = new Client("Valls", "Emmanuel");
 	}
 
 }
