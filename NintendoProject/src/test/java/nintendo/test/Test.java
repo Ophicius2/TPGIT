62b6c36580b2f54ed5f3f1ad87b5047e48d1acde
Ophicius2 <114910037+Ophicius@users.noreply.github.com>
2026-04-30 12:20:09 +0200

    ajout main

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -5,6 +5,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Console c1 = new Console();
+		Console c2 = new Console();
+		Console c3 = new Console();
+		Console c4 = new Console();
+		Console c5 = new Console();
+		
+		Jeu j1 = new Jeu(...);
+		Jeu j2 = new Jeu(...);
+		Jeu j3 = new Jeu(...);
+		Jeu j4 = new Jeu(...);
+		Jeu j5 = new Jeu(...);
+		
 	}
 
 }
