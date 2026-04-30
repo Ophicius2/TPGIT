9e9fa1003c44393345a3c900c159ac5b078dd4f9
GILET Ronan <ronan.gilet@gmail.com>
2026-04-30 12:42:59 +0200

    corr Constructeur dans Salon Hybride et Portable

--- NintendoProject/src/main/java/nintendo/model/Portable.java
+++ NintendoProject/src/main/java/nintendo/model/Portable.java
@@ -1,10 +1,14 @@
 package nintendo.model;
 
+import java.time.LocalDate;
+
 public class Portable extends Console{
 
-	public Portable(String nom) {
-		super(nom);
+	public Portable(String nom,double prix, LocalDate dateDeSortie) {
+		super(nom, prix, dateDeSortie);
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	
 }
--- NintendoProject/src/main/java/nintendo/model/Salon.java
+++ NintendoProject/src/main/java/nintendo/model/Salon.java
@@ -1,9 +1,11 @@
 package nintendo.model;
 
+import java.time.LocalDate;
+
 public class Salon extends Console{
 
-	public Salon(String nom) {
-		super(nom);
+	public Salon(String nom,double prix, LocalDate dateDeSortie) {
+		super(nom, prix, dateDeSortie);
 		// TODO Auto-generated constructor stub
 	}
 
