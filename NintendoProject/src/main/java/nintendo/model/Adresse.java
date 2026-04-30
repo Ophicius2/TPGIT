0775890dc66f9584806353fd78779b39b7c0a69e
Ophicius2 <114910037+Ophicius@users.noreply.github.com>
2026-04-30 12:08:49 +0200

    ajout setter getter

--- NintendoProject/src/main/java/nintendo/model/Adresse.java
+++ NintendoProject/src/main/java/nintendo/model/Adresse.java
@@ -5,4 +5,41 @@ public class Adresse {
 	private String rue;
 	private String ville;
 	
+	
+	public String getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+	public String getRue() {
+		return rue;
+	}
+
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Adresse [numero=" + numero + ", rue=" + rue + ", ville=" + ville + "]";
+	}
+	
 }
+
--- NintendoProject/src/main/java/nintendo/model/Boutique.java
+++ NintendoProject/src/main/java/nintendo/model/Boutique.java
@@ -4,4 +4,22 @@ public class Boutique {
 	private String nom;
 	private Adresse adresse;
 	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Adresse getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", adresse=" + adresse + "]";
+	}
+	
 }
